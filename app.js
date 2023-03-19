b375b8800928c36577cad153976294487a0ddb95
Rejoice Anodo <isaacrejoice011@gmail.com>
2023-03-19 12:45:52 +0100

    adding env

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({mongoUrl: process.env.MONGO_URI,}),
+    //store: MongoStore.create({mongoUrl: process.env.MONGO_URI,}),
   })
 )
 
